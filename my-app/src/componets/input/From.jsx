b599bfe646ae491ed2c0e95ee461404de84a169f
ZobaidulOfficial <zobaidulkazi64.official@gmail.com>
2022-10-16 12:43:33 +0600

    from react button

--- my-app/src/componets/input/From.jsx
+++ my-app/src/componets/input/From.jsx
@@ -4,7 +4,12 @@ const From = (props) => {
 
     <input type={props.type} />
 
-    
+    <div>
+      <h1>My Componets</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae magnam laboriosam nam inventore. Vitae, velit iure earum delectus repellendus quo optio consequatur dolores cumque harum corporis, eum itaque adipisci nam ad necessitatibus doloribus dolor! Aperiam repellendus iure facere rerum magni molestias quae at possimus voluptatum labore accusamus esse quisquam, optio facilis itaque nemo quis! Doloremque reiciendis odio magnam, corrupti distinctio aspernatur perspiciatis qui maxime accusantium tempora in quae, ratione culpa, non quas voluptatum. Repudiandae perferendis accusantium aperiam a. Ab a distinctio magni perspiciatis tempore eaque odio possimus? Nostrum voluptates quis omnis minima magnam quidem dolor iste, vitae deserunt nam aut esse sapiente temporibus, alias facilis fuga numquam, laudantium veniam quo earum ex magni laborum nesciunt neque! Harum repellendus impedit qui veniam adipisci, nemo non, architecto fuga, accusantium quae voluptas id numquam voluptate officiis eveniet minima placeat animi sit? Cum praesentium minus quas tenetur ducimus dolor repudiandae, placeat assumenda blanditiis quia provident molestias aspernatur. Sed, saepe, sit velit optio inventore error, neque esse molestiae nobis ducimus odio culpa corrupti dolores cum nostrum enim sequi. Delectus nobis ex temporibus, ratione esse unde voluptatibus tempora laborum accusantium rerum, aperiam soluta est fugit quidem modi deleniti accusamus blanditiis corporis hic alias iusto sit aspernatur eum. Deleniti alias, iusto atque obcaecati expedita esse exercitationem qui eum quia magni commodi corrupti laboriosam numquam quasi natus vel, dolorem ut deserunt delectus impedit debitis repellendus. Eligendi a blanditiis ullam adipisci? Deleniti qui distinctio, repellat accusantium totam quia quam voluptas officia expedita nobis tempora quidem doloremque vel laborum assumenda architecto iste rem maiores animi harum magni aut. Voluptate id nemo dolor provident, labore architecto harum dignissimos aperiam necessitatibus, rem molestias quasi possimus nesciunt qui nihil quibusdam natus et assumenda quis soluta sapiente nobis pariatur. Alias, necessitatibus quas! Aspernatur praesentium ratione omnis. Neque saepe temporibus velit beatae atque assumenda commodi.</p>
+    </div>
+
+
 
       </div>
   )
